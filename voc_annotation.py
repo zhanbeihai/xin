caa29dc4ddd85da2c96a4abb004ba71598347b1a
zhanbeihai <105540397+zhanbeihai@users.noreply.github.com>
2022-05-28 17:04:47 +0800

    Update voc_annotation.py

--- voc_annotation.py
+++ voc_annotation.py
@@ -32,7 +32,7 @@ train_percent       = 0.9
 #   指向VOC数据集所在的文件夹
 #   默认指向根目录下的VOC数据集
 #-------------------------------------------------------#
-VOCdevkit_path  = '/kaggle/apple1/VOCdevkit'
+VOCdevkit_path  = '/kaggle/input/apple1/VOCdevkit'
 
 VOCdevkit_sets  = [('2007', 'train'), ('2007', 'val')]
 classes, _      = get_classes(classes_path)
